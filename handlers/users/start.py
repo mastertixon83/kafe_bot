cce03fa21da7e44c6820d28857a79e97cf462464
Andrey Tikhonov <mastertixon.is@gmail.com>
2023-03-23 22:54:39 +0600

    Finished

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,4 +1,5 @@
 #TODO: Написать инструкцию по пользованию ботом
+#TODO: !!!Сделать логирование
 from keyboards.inline.dating_ikb import user_gender_ikb, user_work_ikb
 from loader import dp, bot, db
 
